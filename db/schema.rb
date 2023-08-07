d6da1c8e1eb7f5c77d9d01c4f5f7ff74195681cb
Harriet Oteng <harriet.oteng@gmail.com>
2023-08-07 20:12:05 +0000

    Add reference fields

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_08_07_183301) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_07_200742) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -47,12 +47,20 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_07_183301) do
     t.decimal "amount"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "author_id", null: false
+    t.bigint "group_id", null: false
+    t.index ["author_id"], name: "index_expenses_on_author_id"
+    t.index ["group_id"], name: "index_expenses_on_group_id"
   end
 
   create_table "groups", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id", null: false
+    t.bigint "expense_id", null: false
+    t.index ["expense_id"], name: "index_groups_on_expense_id"
+    t.index ["user_id"], name: "index_groups_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -70,4 +78,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_07_183301) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "expenses", "groups"
+  add_foreign_key "expenses", "users", column: "author_id"
+  add_foreign_key "groups", "expenses"
+  add_foreign_key "groups", "users"
 end
